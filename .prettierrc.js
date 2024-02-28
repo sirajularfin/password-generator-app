5634aea25ed29ce59ba6eddf3da34501b9bf9326
Sirajul Arfin <sirajul.arfin@nagarro.com>
2024-02-28 19:34:54 +0530

    Update changes to settings file.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  printWidth: 120,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: 'all',
+  useTabs: true,
+};
